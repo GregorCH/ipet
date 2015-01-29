e55bbd5a640a288dde35b34216435d732f44be83
Gregor Hendel <hendel@zib.de>
2015-01-29 14:53:29 +0100

    enabled parsing of arguments in startipet.py

--- scripts/startipet.py
+++ scripts/startipet.py
@@ -4,7 +4,36 @@ Created on 02.08.2014
 @author: Gregor Hendel
 '''
 from ipet.IpetApplication import IpetApplication
+from ipet.Comparator import Comparator
+import argparse
+import sys
+
+# possible arguments in the form name,default,short,description #
+clarguments = [('--comparatorfile', None,'-c', "A comparator file name (must have .cmp file extension) in cmp-format to read")]
+
+argparser = argparse.ArgumentParser(prog="Ipet Startup Script", \
+                                 description="starts the IPET graphical user interface")
+for name, default, short, description in clarguments:
+    argparser.add_argument(short, name, default=default,help=description)
+
+
+
 
 if __name__ == '__main__':
-    gui = IpetApplication()
+    try:
+        n = vars(argparser.parse_args())
+        globals().update(n)
+    except:
+        print "Wrong Usage"
+    #if globals().get("help") is not None:
+
+    #initialize a comparator
+    comparator = None
+    if comparatorfile is not None:
+        try:
+            comparator = Comparator.loadFromFile(comparatorfile)
+        except NameError:
+            pass
+
+    gui = IpetApplication(comparator)
     gui.mainloop()

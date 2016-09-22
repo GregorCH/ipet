24ab9bdb11b5b227a543686d4c08db65220cb296
Gregor Hendel <hendel@zib.de>
2016-09-22 17:49:25 +0200

    new Qt structures for QIPET application

---
+++ qipet/IPETParserWindow.py
@@ -0,0 +1,133 @@
+'''
+Created on 21.09.2016
+
+@author: bzfhende
+'''
+from IpetMainWindow import IpetMainWindow
+from PyQt4.QtGui import QLayout, QHBoxLayout
+from PyQt4.Qt import QVBoxLayout, QWidget, QFrame, QTextEdit, QApplication, QFileDialog, QString
+from IPetTreeView import IpetTreeView
+import sys
+from qipet.EditableBrowser import EditableBrowser
+from IPETApplicationTab import IPETApplicationTab
+from ipet.parsing import ReaderManager
+from ipet import misc
+from ipet.parsing.StatisticReader_CustomReader import CustomReader
+from ipet.parsing.StatisticReader import ListReader
+
+class IPETParserWindow(IPETApplicationTab):
+
+    naddedreaders = 0
+    def __init__(self, parent = None):
+        super(IPETApplicationTab, self).__init__(parent)
+
+        vlayout = QVBoxLayout()
+        self.editablebrowser = EditableBrowser()
+        vlayout.addWidget(self.editablebrowser)
+        vlayout.addWidget(QTextEdit(self))
+
+        self.setLayout(vlayout)
+        self.parser = None
+        self.filename = None
+        self.defineActions()
+
+    def setParser(self, parser):
+        self.editablebrowser.setRootElement(parser)
+        self.parser = parser
+
+    def defineActions(self):
+        self.loadaction = self.createAction("&Load parser", self.loadParser, None, icon = "Load-icon",
+                                       tip = "Load parser from XML file (current parser gets discarded)")
+        self.saveaction = self.createAction("Save parser", self.saveParser, None, icon = "disk-icon",
+                                        tip = "Save parser to a file")
+        self.saveasaction = self.createAction("Save parser", self.saveParserAs, None, icon = "disk-icon",
+                                        tip = "Save parser to a file of a specified name")
+
+        self.addcustomreaderaction = self.createAction("Add Custom Reader", self.addCustomReader, None, icon = "3d-glasses-icon",
+                                                       tip = "Add a custom reader to the parser")
+        self.addlistreaderaction = self.createAction("Add List Reader", self.addListReader, "Alt+L", icon = "list-icon",
+                                                      tip = "Add a list reader to this parser")
+
+    def getMenuActions(self):
+        return (("&File", [self.loadaction, self.saveaction, self.saveasaction]), ("&Readers", [self.addcustomreaderaction, self.addlistreaderaction]))
+
+    def getToolBarActions(self):
+        return (("&File", [self.loadaction, self.saveaction, self.saveasaction]), ("&Readers", [self.addcustomreaderaction, self.addlistreaderaction]))
+
+    def loadParser(self):
+        thedir = unicode(".")
+        filename = unicode(QFileDialog.getOpenFileName(self, caption = QString("%s - Load a parser" % QApplication.applicationName()),
+                                               directory = thedir, filter = unicode("XML files (*.xml)")))
+        if filename:
+            try:
+                parser = ReaderManager.fromXMLFile(filename)
+                message = "Loaded parser from %s" % filename
+                self.setParser(parser)
+            except Exception:
+                message = "Error: Could not load parser from file %s" % filename
+
+            self.updateStatus(message)
+
+    def addCustomReader(self):
+        self.naddedreaders += 1
+        reader = CustomReader("New Custom Reader %d" % self.naddedreaders, "regpattern", "datakey")
+        self.editablebrowser.addNewElementAsChildOfSelectedElement(reader)
+
+    def addListReader(self):
+        self.naddedreaders += 1
+        reader = ListReader("cleverRegpattern", name = "new List Reader %d" % self.naddedreaders)
+        self.editablebrowser.addNewElementAsChildOfSelectedElement(reader)
+
+
+    def saveParser(self):
+        if self.filename is None:
+            filename = unicode(QFileDialog.getSaveFileName(self, caption = QString("%s - Save a parser" % QApplication.applicationName()),
+                                                           directory = unicode("."), filter = unicode("XML files (*.xml)")))
+        else:
+            filename = self.filename
+
+        if not filename:
+            return
+
+
+        misc.saveAsXML(self.parser, filename)
+        self.filename = filename
+        self.updateStatus("Saved parser to file %s" % filename)
+
+
+    def saveParserAs(self):
+        filename = unicode(QFileDialog.getSaveFileName(self, caption = QString("%s - Save a parser" % QApplication.applicationName()),
+                                                       directory = unicode("."), filter = unicode("XML files (*.xml)")))
+        if not filename:
+            return
+
+        misc.saveAsXML(self.parser, filename)
+        self.filename = filename
+        self.updateStatus("Saved parser to file %s" % filename)
+#
+#     def enableOrDisableActions(self):
+#         for action, addclass in zip([self.addcolaction, self.addfiltergroupaction, self.addfilteraction, self.addaggregationaction, self.addinstancenaction],
+#                                     [IPETEvaluationColumn(), IPETFilterGroup(), IPETFilter(), Aggregation(), IPETInstance()]):
+#             if self.browser.treewidget.currentItemAcceptsClassAsChild(addclass):
+#                 action.setEnabled(True)
+#             else:
+#                 action.setEnabled(False)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    imw = IpetMainWindow()
+    imw.setWindowTitle("Parser Window")
+
+    app.setApplicationName("Evaluation editor")
+
+    readermanager = ReaderManager.fromXMLFile("../scripts/readers-example.xml")
+
+    parserwindow = IPETParserWindow()
+    parserwindow.setParser(readermanager)
+    imw.setCentralWidget(parserwindow)
+    imw.populateMenu(parserwindow)
+    imw.populateToolBar(parserwindow)
+
+    imw.show()
+
+    sys.exit(app.exec_())
---
+++ qipet/QIPETApplication.py
@@ -0,0 +1,42 @@
+'''
+Created on 22.09.2016
+
+@author: Gregor Hendel
+'''
+
+from IpetMainWindow import IpetMainWindow
+from PyQt4.QtGui import QLayout, QHBoxLayout
+from PyQt4.Qt import QVBoxLayout, QWidget, QFrame, QTextEdit, QApplication, QTabWidget
+from IPetTreeView import IpetTreeView
+import sys
+from EditableBrowser import EditableBrowser
+from IPETParserWindow import IPETParserWindow
+from IpetEvaluationEditorApp import EvaluationEditorWindow
+
+class QIPETApplication(IpetMainWindow):
+    '''
+    an application that presents different widgets for parsing and evaluating in tabs
+    '''
+
+    def __init__(self, parent = None):
+        super(QIPETApplication, self).__init__(parent)
+
+        tabwidget = QTabWidget()
+        tabwidget.addTab(IPETParserWindow(), "Parser")
+        tabwidget.addTab(EvaluationEditorWindow(), "Evaluation")
+
+        self.setCentralWidget(tabwidget)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    qipetapplication = QIPETApplication()
+    qipetapplication.setWindowTitle("IPET 2.0")
+
+    app.setApplicationName("Evaluation editor")
+    parserwindow = IPETParserWindow()
+
+    qipetapplication.show()
+
+    sys.exit(app.exec_())

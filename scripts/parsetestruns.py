8d9b24f3e5d2defd7cd1745b1dedf02e3cf23cbb
Stephen J Maher <maher@zib.de>
2016-09-22 11:48:23 +0200

    added flag for log files in parsetestruns

--- scripts/parsetestruns.py
+++ scripts/parsetestruns.py
@@ -21,7 +21,7 @@ argparser = argparse.ArgumentParser(prog = "Ipet Parsing script", \
 for name, default, short, description in clarguments:
     argparser.add_argument(short, name, default=default,help=description)
 
-argparser.add_argument('outfiles', nargs='*', help="list of outfiles that should be parsed")
+argparser.add_argument('-l','--logfiles', nargs='*', help="list of outfiles that should be parsed")
 argparser.add_argument('-r','--readers', nargs='*', default=[], help="list of additional readers in xml format that should be used for parsing")
 argparser.add_argument('-s','--solufiles', nargs='*', default=[], help="list of solu files that should be taken into account")
 argparser.add_argument("-D", "--debug", action = "store_true", default = False, help = "Enable debug output to console during parsing")
@@ -37,7 +37,7 @@ if __name__ == '__main__':
         exit()
     #if globals().get("help") is not None:
     print globals()
-    if outfiles is None:
+    if logfiles is None:
         print "We need out files"
         sys.exit(0)
 
@@ -58,8 +58,8 @@ if __name__ == '__main__':
         for reader in rm.getManageables(False):
             experiment.readermanager.registerReader(reader)
 
-    for outfile in outfiles:
-        experiment.addOutputFile(outfile)
+    for logfile in logfiles:
+        experiment.addOutputFile(logfile)
 
     for solufile in solufiles:
         experiment.addSoluFile(solufile)

219ee2ad790a89e09d21a1c78cd36c68c8277c11
Gregor Hendel <hendel@zib.de>
2016-09-23 10:12:23 +0200

    Trace File reader

---
+++ ipet/parsing/TraceFileReader.py
@@ -0,0 +1,51 @@
+'''
+Created on 21.09.2016
+
+@author: bzfhende
+'''
+from StatisticReader import StatisticReader
+from ipet.misc import FLOAT_INFINITY
+import logging
+class TraceFileReader(StatisticReader):
+    '''
+    classdocs
+    '''
+    active = False
+    context = StatisticReader.CONTEXT_TRACEFILE
+    tracefilestartexpression = "* Trace Record Definition"
+    input = "InputFileName,ModelType,SolverName,OptionFile,Direction,NumberOfEquations,NumberOfVariables,NumberOfDiscreteVariables,NumberOfNonZeros,NumberOfNonlinearNonZeros," + \
+            "ModelStatus,SolverStatus,ObjectiveValue,ObjectiveValueEstimate,SolverTime,ETSolver,NumberOfIterations,NumberOfNodes"
+    datakeys = input.split(",")[1:]
+    
+    floatmap = {"NA":None,
+                "-INF":-FLOAT_INFINITY,
+                "+INF":+FLOAT_INFINITY
+                }
+
+    def prepareData(self, value):
+        value = self.floatmap.get(value, value)
+        if value is None:
+            return None
+
+        for datatype in (int, float):
+            try:
+                return datatype(value)
+            except ValueError:
+                pass
+
+        return value
+
+
+    def extractStatistic(self, line):
+        if line.startswith(self.tracefilestartexpression):
+            self.active = True
+        elif self.active and not line.startswith("*"):
+            splitline = line.split(",")
+            probname = splitline[0]
+            datavalues = map(self.prepareData, splitline[1:])
+
+            logging.debug("Trace File Reader adds data for problem %s", probname)
+            self.testrun.addData(probname, self.datakeys, datavalues)
+
+
+

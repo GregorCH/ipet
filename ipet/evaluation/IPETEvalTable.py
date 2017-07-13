46908b37c70005ee1a2291f22b1617be9cf2a5bb
Franziska Schlösser <schloesser@zib.de>
2017-07-13 13:35:20 +0200

    fix messages from debug to info, hide 'if' in generateIndex

--- ipet/evaluation/IPETEvalTable.py
+++ ipet/evaluation/IPETEvalTable.py
@@ -1173,6 +1173,7 @@ class IPETEvaluation(IpetNode):
 
         # take the tuple with the highest occurrence
         self.orig_defaultgroup = ":".join(max_key)
+        logging.info("Generated defaultgroup to {}".format(self.orig_defaultgroup))
 
     def generateIndex(self, data):
         '''
@@ -1190,32 +1191,36 @@ class IPETEvaluation(IpetNode):
         data
             the data of the experiment
         '''
-        lowerbound = 1 # 1 or bigger
-        possible_indices = [Key.ProblemName, Key.Solver, Key.Settings, Key.Version, Key.LogFileName]
-        height = data.shape[0]
+        # do this only if the user requested an automatic index
+        if self.autoIndex:
+            lowerbound = 1 # 1 or bigger
+            possible_indices = [Key.ProblemName, Key.Solver, Key.Settings, Key.Version, Key.LogFileName]
+            height = data.shape[0]
+
+            # find the indices that are represented in the data with their numbers of unique values
+            present_indices = [[key, data[key].nunique()] for key in possible_indices if key in data.columns]
+            # take the index with the max value of the previous as rowindex
+            first = max(present_indices, key = lambda y: y[1])
+
+            processed_indices = [[key, count] for [key, count] in present_indices if count > lowerbound and key != first[0]]
+            sorted_indices = sorted(processed_indices, key = lambda y: y[1])
+
+            # try to find a columnindex
+            second = []
+            if len(sorted_indices) > 0 and sorted_indices[0][0] != first[0]:
+                second = [sorted_indices[0]]
+                # check if a second columnindex can be helpful
+                if len(sorted_indices) > 1 and (height / first[1]) / second[0][1] > 1:
+                    second.append(sorted_indices[1])
+
+            # set everything
+            self.indexsplit = 1
+            self.generateDefaultGroup(data, [i[0] for i in second])
+            self.set_index(" ".join([i[0] for i in [first] + second]))
+            logging.info("Automatically set index to ({}, {})".format(self.getRowIndex(), self.getColIndex()))
+        elif self.orig_defaultgroup == "":
+            self.generateDefaultGroup(data, list(self.getColIndex()))
         
-        # find the indices that are represented in the data with their numbers of unique values
-        present_indices = [[key, data[key].nunique()] for key in possible_indices if key in data.columns]
-        # take the index with the max value of the previous as rowindex
-        first = max(present_indices, key = lambda y: y[1])
-
-        processed_indices = [[key, count] for [key, count] in present_indices if count > lowerbound and key != first[0]]
-        sorted_indices = sorted(processed_indices, key = lambda y: y[1])
-        
-        # try to find a columnindex
-        second = []
-        if len(sorted_indices) > 0 and sorted_indices[0][0] != first[0]:
-            second = [sorted_indices[0]]
-            # check if a second columnindex can be helpful
-            if len(sorted_indices) > 1 and (height / first[1]) / second[0][1] > 1:
-                second.append(sorted_indices[1])
-
-        # set everything
-        self.indexsplit = 1
-        self.generateDefaultGroup(data, [i[0] for i in second])
-        self.set_index(" ".join([i[0] for i in [first] + second]))
-        logging.debug("Automatically set index to ({}, {})".format(self.getRowIndex(), self.getColIndex()))
-
     def evaluate(self, exp : Experiment):
         """
         evaluate the data of an Experiment instance exp
@@ -1238,10 +1243,7 @@ class IPETEvaluation(IpetNode):
         data = exp.getJoinedData()
         logging.debug("Result of getJoinedData:\n{}\n".format(data))
 
-        if self.autoIndex:
-            self.generateIndex(data)
-        elif self.orig_defaultgroup == "":
-            self.generateDefaultGroup(data, list(self.getColIndex()))
+        self.generateIndex(data)
 
         if not self.groupkey in data.columns:
             raise KeyError(" Group key is missing in data:", self.groupkey)
